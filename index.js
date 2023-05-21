26bc90c4a006c45ade5a85c1d74fa6e01d8494b8
Romeo-Richardson <mrculatr101@outlook.com>
2023-05-21 07:07:14 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -1,52 +1,68 @@
-const express = require('express');
-const mongoose = require('mongoose')
-const gtts = require('node-gtts')('en');
+const express = require("express");
+const mongoose = require("mongoose");
+const gtts = require("node-gtts")("en");
+const path = require("path");
+require("dotenv").config();
 const app = express();
-app.use(express.json())
-const cors = require('cors')
+app.use(express.json());
+const cors = require("cors");
 const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions))
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
-const historyModel = require('./models/history')
+const historyModel = require("./models/history");
 
-mongoose.connect('mongodb+srv://ZirrKing:65937675299041230290728123677583@cluster0.5r4pdz7.mongodb.net/aigod?retryWrites=true&w=majority')
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://ZirrKing:65937675299041230290728123677583@cluster0.5r4pdz7.mongodb.net/aigod?retryWrites=true&w=majority"
+);
 
-app.get('/getHistory', async (req, res) => {
-    const history = await historyModel.find()
-    res.json(history)
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/ai-react/build")));
 
-app.get('/speech', function (req, res) {
-    res.set({ 'Content-Type': 'audio/mpeg' });
+  app.get("/getHistory", async (req, res) => {
+    const history = await historyModel.find();
+    res.json(history);
+  });
+
+  app.get("/speech", function (req, res) {
+    res.set({ "Content-Type": "audio/mpeg" });
     gtts.stream(req.query.text).pipe(res);
-})
+  });
 
-app.post('/postUser', async (req, res) => {
-    const user = req.body
-    const newUser = new historyModel(user)
-    await newUser.save()
-    res.json(newUser)
-})
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "ai-react", "build", "index.html"));
+  });
+} else
+  app.get("/", (req, res) => {
+    res.send("api running");
+  });
 
-app.put('/postHistory', async (req, res) => {
-    const id = req.body._id
-    const newHistory = req.body.history
-    try {
-        const getHistory = await  historyModel.findById(id)
-        console.log(getHistory)
-        getHistory.history = newHistory
-        await getHistory.save()
-        res.status(200).json(getHistory)
-    } catch (error) {
-        console.log(error)
-        console.log(req.body)
-    }
-})
+app.post("/postUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new historyModel(user);
+  await newUser.save();
+  res.json(newUser);
+});
 
-app.listen(3001, () => {
-    console.log('server running on port 3001')
-})
+app.put("/postHistory", async (req, res) => {
+  const id = req.body._id;
+  const newHistory = req.body.history;
+  try {
+    const getHistory = await historyModel.findById(id);
+    console.log(getHistory);
+    getHistory.history = newHistory;
+    await getHistory.save();
+    res.status(200).json(getHistory);
+  } catch (error) {
+    console.log(error);
+    console.log(req.body);
+  }
+});
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`server running on port ${process.env.PORT}`);
+});
